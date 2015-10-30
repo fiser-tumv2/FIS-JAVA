b0f3511571b5eb8838f50e11fd467874039ca51a
fiser-tumv2 <linkjsdtq@gmail.com>
2015-10-30 23:07:02 +0700

    Project Symfony Demo

---
+++ Symfony FrameWork/LoginProject/app/cache/dev/twig/a3/f2/576d5722f845768286e141aef917.php
@@ -0,0 +1,87 @@
+<?php
+
+/* SensioDistributionBundle::Configurator/layout.html.twig */
+class __TwigTemplate_a3f2576d5722f845768286e141aef917 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\" />
+        <link rel=\"stylesheet\" href=\"";
+        // line 5
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/sensiodistribution/webconfigurator/css/configure.css"), "html", null, true);
+        echo "\" />
+        <title>";
+        // line 6
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        <link rel=\"icon\" type=\"image/x-icon\" sizes=\"16x16\" href=\"";
+        // line 7
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/sensiodistribution/webconfigurator/images/favicon.ico"), "html", null, true);
+        echo "\" />
+    </head>
+    <body>
+        <div id=\"symfony-wrapper\">
+            <div id=\"symfony-header\">
+                <img src=\"";
+        // line 12
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/sensiodistribution/webconfigurator/images/logo-small.gif"), "html", null, true);
+        echo "\" alt=\"Symfony logo\" />
+            </div>
+            <div id=\"symfony-content\">
+                ";
+        // line 15
+        $this->displayBlock('content', $context, $blocks);
+        // line 16
+        echo "            </div>
+            <div class=\"version\">Symfony Standard Edition v.";
+        // line 17
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "</div>
+        </div>
+    </body>
+</html>
+";
+    }
+
+    // line 6
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Web Configurator Bundle";
+    }
+
+    // line 15
+    public function block_content($context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "SensioDistributionBundle::Configurator/layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  69 => 15,  63 => 6,  54 => 17,  51 => 16,  43 => 12,  35 => 7,  27 => 5,  21 => 1,  115 => 40,  112 => 39,  110 => 38,  107 => 37,  101 => 33,  92 => 31,  88 => 30,  82 => 26,  78 => 24,  74 => 22,  72 => 21,  67 => 18,  65 => 17,  62 => 16,  58 => 14,  49 => 15,  45 => 11,  38 => 7,  34 => 5,  31 => 6,  28 => 3,);
+    }
+}
